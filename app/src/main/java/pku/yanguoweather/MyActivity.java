925b021391fa11800fbf1ec6c7065dffc86b88cc
farewellfirefly <wangyanguo@pku.edu.cn>
2018-09-27 10:01:42 +0800

    first commit

---
+++ app/src/main/java/pku/yanguoweather/MyActivity.java
@@ -0,0 +1,15 @@
+package pku.yanguoweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        setContentView(R.layout.yanguo_layout);
+
+    }
+}
